ed69b01567d14419c007dde4764228c384ab2238
DiogoTiaen <diogorstiaen@gmail.com>
2025-11-13 18:09:41 -0300

    commit spring

---
+++ HabiTrackAPI/src/main/java/com/habitrack/HabiTrackAPI/HabiTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.habitrack.HabiTrackAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabiTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabiTrackApiApplication.class, args);
+	}
+
+}
